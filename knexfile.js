621dc9e0e8decdcff13dc0b82d5d889aad4e637b
anmolhanda <anmolhanda@techsmarters@gmail.com>
2025-05-22 11:43:53 +0530

    update

--- knexfile.js
+++ knexfile.js
@@ -1,34 +1,37 @@
-require('dotenv').config();
+require("dotenv").config();
 
 module.exports = {
   development: {
-    client: 'mysql2',
+    client: "mysql2",
     connection: {
-      host: process.env.DB_HOST,
-      user: process.env.DB_USER,
-      password: process.env.DB_PASS,
-      database: process.env.DB_NAME
+      host:
+        process.env.DB_HOST ||
+        "bzmgwmkemfb8fxh7d35k-mysql.services.clever-cloud.com",
+      user: process.env.DB_USER || "ukdmygt7iiy1tayu",
+      password: process.env.DB_PASS || "SDTGZm5W2XlHGgMNQ9JE",
+      database: process.env.DB_NAME || "bzmgwmkemfb8fxh7d35k",
     },
+
     migrations: {
-      directory: './migrations'
+      directory: "./migrations",
     },
     seeds: {
-      directory: './seeds'
-    }
+      directory: "./seeds",
+    },
   },
   production: {
-    client: 'mysql2',
+    client: "mysql2",
     connection: {
       host: process.env.DB_HOST,
       user: process.env.DB_USER,
       password: process.env.DB_PASS,
-      database: process.env.DB_NAME
+      database: process.env.DB_NAME,
     },
     migrations: {
-      directory: './migrations'
+      directory: "./migrations",
     },
     seeds: {
-      directory: './seeds'
-    }
-  }
+      directory: "./seeds",
+    },
+  },
 };
